2ea00aa3631882ca5afae2cd91345c69a41e2266
beer <arriya3567@gmail.com>
2022-11-04 17:34:11 +0700

    run4

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import liff from '@line/liff';
+import 'antd/dist/antd.css';
 import { Button } from 'antd';
 
 export default function Home() {
