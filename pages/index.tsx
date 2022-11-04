18da3857313026e385925634350a78b8e9d37da1
beer <arriya3567@gmail.com>
2022-11-04 17:19:08 +0700

    run2

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,13 +6,13 @@ import liff from '@line/liff';
 export default function Home() {
   return (
     <div>
-     
-  <h1>Angular with LINE Login</h1>
-  <b>id token: </b> 
-  <b>display name: </b>
-  <b>status message: </b> 
- <b>user id: </b> 
-<button>Logout</button>
+
+      <h1 >Angular with LINE Login</h1>
+      <b>id token: </b>
+      <b>display name: </b>
+      <b>status message: </b>
+      <b>user id: </b>
+      <button>Logout</button>
 
     </div>
   )
