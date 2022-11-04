b54a866b6224193ad1289b6904f351d5109e8450
beer <arriya3567@gmail.com>
2022-11-04 17:03:15 +0700

    run1

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,19 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import liff from '@line/liff';
 
 export default function Home() {
   return (
     <div>
-      aaaaaaa
+     
+  <h1>Angular with LINE Login</h1>
+  <b>id token: </b> 
+  <b>display name: </b>
+  <b>status message: </b> 
+ <b>user id: </b> 
+<button>Logout</button>
+
     </div>
   )
 }
