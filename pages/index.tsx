8570b860af0d1585613fe226cef1b6f667221e79
beer <arriya3567@gmail.com>
2022-11-07 10:23:48 +0700

    run

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,24 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import liff from '@line/liff';
+import React from 'react';
+import { Input } from 'antd';
 import 'antd/dist/antd.css';
 import { Button } from 'antd';
 
+const App: React.FC = () => <Input placeholder="Basic usage" />;
 
-export default function Home() {
-  return (
-    <div>
-      <h1 >Angular with LINE Login</h1>
-      <h1 >Angular with LINE Login</h1>
-      <b>id token: </b>
-      <b>display name: </b>
-      <b>status message: </b>
-      <b>user id: </b>
-      <Button type="primary">Logout</Button>
+export default App;
 
-    </div>
-  )
-}
+// export default function Home() {
+//   return (
+//     <div>
+//       {/* <h1 >Angular with LINE Login</h1>
+//       <h1 >Angular with LINE Login</h1>
+//       <b>id token: </b>
+//       <b>display name: </b>
+//       <b>status message: </b>
+//       <b>user id: </b>
+//       <Button type="primary" htmlType="submit"> */}
+      
+
+//     </div>
+//   )
+// }
