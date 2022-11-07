c6a61e76fc18ca8a7b0b676d2737e7b593a4f3b0
beer <arriya3567@gmail.com>
2022-11-07 14:43:58 +0700

    run 1.7

--- pages/profile.js
+++ pages/profile.js
@@ -4,30 +4,44 @@ import Image from 'next/image'
 
 
 export default function Profile() {
-  const [profile, setProfile] = useState({})
+    const [profile, setProfile] = useState({})
+    let liff
+    useEffect(() => {
+        liff = require('@line/liff')
+        await liff.ready
+        const lineliff = async () => {
+            try {
+                await liff.init({ liffId: `${process.env.NEXT_PUBLIC_LIFF_ID}` });
+            } catch (error) {
+                console.error('liff init error', error.message)
+            }
+            if (!liff.isLoggedIn()) {
+                liff.login();
+            }
+            const profile = await liff.getProfile()
+            setProfile(profile)
+        }
+        lineliff()
+    })
 
-  useEffect(async () => {
-    const liff = (await import('@line/liff')).default
-    await liff.ready
-    const profile = await liff.getProfile()
-    setProfile(profile)
-  }, [profile.userId])
+
+  
 
   return (
-    <section>
-      <Head>
-        <title>My Profile</title>
-      </Head>
-      <h1>Profile</h1>
-      <div>
-        {profile.pictureUrl && <Image
-          src={profile.pictureUrl}
-          alt={profile.displayName}
-          width={500}
-          height={500}
-        />}
-        <div>Name: {profile.displayName}</div>
-      </div>
-    </section>
-  )
+        <section>
+            <Head>
+                <title>My Profile</title>
+            </Head>
+            <h1>Profile</h1>
+            <div>
+                {profile.pictureUrl && <Image
+                    src={profile.pictureUrl}
+                    alt={profile.displayName}
+                    width={500}
+                    height={500}
+                />}
+                <div>Name: {profile.displayName}</div>
+            </div>
+        </section>
+    )
 }
