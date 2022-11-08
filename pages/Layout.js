82946587a4a2853f531612b0497c747ae936b048
beer <arriya3567@gmail.com>
2022-11-08 10:45:01 +0700

    la10

--- pages/Layout.js
+++ pages/Layout.js
@@ -12,8 +12,19 @@ const { Header, Sider, Content } = Layout;
 const App = () => {
     const [collapsed, setCollapsed] = useState(false);
     return (
-        <Layout>
-            <Sider trigger={null} collapsible collapsed={collapsed}>
+            <Layout className="site-layout">
+                <Header
+                    className="site-layout-background"
+                    style={{
+                        padding: 0,
+                    }}
+                >
+                    {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+                        className: 'trigger',
+                        onClick: () => setCollapsed(!collapsed),
+                    })}
+                </Header>
+                <Sider trigger={null} collapsible collapsed={collapsed}>
                 <div className="logo" />
                 <Menu
                     theme="dark"
@@ -38,18 +49,6 @@ const App = () => {
                     ]}
                 />
             </Sider>
-            <Layout className="site-layout">
-                <Header
-                    className="site-layout-background"
-                    style={{
-                        padding: 0,
-                    }}
-                >
-                    {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
-                        className: 'trigger',
-                        onClick: () => setCollapsed(!collapsed),
-                    })}
-                </Header>
                 <Content
                     className="site-layout-background"
                     style={{
