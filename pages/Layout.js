fd7e46c35f2894ef5f395ceb7d04be5adc82e937
beer <arriya3567@gmail.com>
2022-11-08 11:44:28 +0700

    la17

--- pages/Layout.js
+++ pages/Layout.js
@@ -33,15 +33,14 @@ const items = [
 const App = () => {
     const [collapsed, setCollapsed] = useState(false);
     return (
-        <Layout
-            style={{
-                minHeight: '100vh',
-            }}
-        >
+        <Layout style={{minHeight: '100vh',}}>
+
             <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
                 <div className="logo" />
-                <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} />
+                <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} /> 
             </Sider>
+            {/*Sider แถบด้านข้าง จะทำการประกาศ items เมนูไว้ข้างบนแล้วดึงมาใช้ ใน <Menu/>*/}
+
             <Layout className="site-layout">
                 <Header>
                     <div className="logo" />
@@ -58,36 +57,23 @@ const App = () => {
                         })}
                     />
                 </Header>
-                <Content
-                    style={{
-                        margin: '0 16px',
-                    }}
-                >
-                    <Breadcrumb
-                        style={{
-                            margin: '16px 0',
-                        }}
-                    >
+                {/*Header แถบด้านบน กำหนดจำนวนโดยใช้  Array(X) ว่าต้องการเมนูอีกตัว */}
+
+                <Content style={{  margin: '0 16px',}}>
+                    <Breadcrumb style={{ margin: '16px 0', }} >
                         <Breadcrumb.Item>User</Breadcrumb.Item>
                         <Breadcrumb.Item>Bill</Breadcrumb.Item>
                     </Breadcrumb>
-                    <div
-                        className="site-layout-background"
-                        style={{
-                            padding: 40,
-                            minHeight: 460,
-                        }}
-                    >
+                    <div className="site-layout-background" style={{ padding: 40,minHeight: 460,}}>
                         Bill is a cat.
                     </div>
                 </Content>
-                <Footer
-                    style={{
-                        textAlign: 'center',
-                    }}
-                >
+                 {/*Content ส่วนเนื้อตรงกลาง */}
+
+                <Footer style={{ textAlign: 'center', }} >
                     Build ตอนไหนตื่นเต้นทุกที่
                 </Footer>
+
             </Layout>
         </Layout>
     );
