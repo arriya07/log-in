4c5935541b77ed8d60fb98d1713a79bb65c4cec4
beer <arriya3567@gmail.com>
2022-11-08 15:56:02 +0700

    la 39

--- pages/Layout.js
+++ pages/Layout.js
@@ -1,29 +1,77 @@
 import 'antd/dist/antd.css'
-import React from 'react';
-import ReactDOM from 'react-dom';
+import {
+    TeamOutlined,
+    UserOutlined,
+} from '@ant-design/icons';
+import { Breadcrumb, Layout, Menu } from 'antd';
+import React, { useState } from 'react';
+import Profile from './profile';
 
-import { Provider } from 'react-redux';
-import { BrowserRouter, Switch } from 'react-router-dom';
 
-import RouteApp from './components/App';
-import getStore from './store/getStore';
 
-import './index.css';
-import Users from './components/Users';
-import Counter from './components/Counter';
-import Home from './components/Home';
+const { Header, Content, Footer, Sider } = Layout;
+function getItem(label,key, icon, children,) {
+    return {
+        icon,
+        children,
+        label,
+        key,
+    };
+}
+const items = [
+    getItem('NemeUser','0',),
+    getItem('Usere Profil','./profile',<UserOutlined />),
+    getItem('Team', 'sub2', <TeamOutlined />, 
+    [getItem('Team 1', '6'), 
+    getItem('Team 2', '8')]),
 
-const { store } = getStore();
+];
+const App = () => {
+    const [collapsed, setCollapsed] = useState(false);
+    return (
+        <Layout style={{minHeight: '100vh',}}>
 
-ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <Switch>
-        <RouteApp path={'/users'} component={Users} />
-        <RouteApp path={'/counter'} component={Counter} />
-        <RouteApp path={'/'} component={Home} />
-      </Switch>
-    </BrowserRouter>
-  </Provider>,
-  document.getElementById('root')
-);
+            <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
+                <div className="logo" />
+                <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} /> 
+            </Sider>
+            {/*Sider แถบด้านข้าง จะทำการประกาศ items เมนูไว้ข้างบนแล้วดึงมาใช้ ใน <Menu/>*/}
+
+            <Layout className="site-layout">
+                {/* <Header>
+                    <div className="logo" />
+                    <Menu
+                        theme="dark"
+                        mode="horizontal"
+                        defaultSelectedKeys={['2']}
+                        items={new Array().fill(null).map((_, index) => {
+                            const key = index + 1;
+                            return {
+                                key,
+                                label: ` ${key}`,
+                            };
+                        })}
+                    />
+                </Header> */}
+                {/*Header แถบด้านบน กำหนดจำนวนโดยใช้  Array(X) ว่าต้องการเมนูอีกตัว */}
+
+                <Content style={{  margin: '0 16px',}}>
+                    <Breadcrumb style={{ margin: '16px 0', }} >
+                        <Breadcrumb.Item>User</Breadcrumb.Item>
+                        <Breadcrumb.Item>Bill</Breadcrumb.Item>
+                    </Breadcrumb>
+                    <div className="site-layout-background" style={{ padding: 40,minHeight: 460,}}>
+                        Bill is a cat.
+                    </div>
+                </Content>
+                 {/*Content ส่วนเนื้อตรงกลาง */}
+
+                <Footer style={{ textAlign: 'center', }} >
+                    Build ตอนไหนตื่นเต้นทุกที่
+                </Footer>
+
+            </Layout>
+        </Layout>
+    );
+};
+export default App;
