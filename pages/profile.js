1b48688e56f46fae06373aebc66f47a9f5f55115
beer <arriya3567@gmail.com>
2022-11-09 09:42:31 +0700

    la 2.3

--- pages/profile.js
+++ pages/profile.js
@@ -1,6 +1,18 @@
 import { useEffect, useState } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
+import 'antd/dist/antd.css'
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from '@ant-design/icons';
+import { Layout, Menu } from 'antd';
+import React, { useState } from 'react';
+import Link from 'next/link'
+const { Header, Sider, Content } = Layout;
 
 export default function Profile() {
     const [profile, setProfile] = useState({}) //useState เป็นการเรียกใช้งาน state และ การเปลี่ยนแปลงค่า state
@@ -22,12 +34,54 @@ export default function Profile() {
         lineliff()
     })
 
-
-  
-
   return (
         <section>
             <div style={{ textAlign: "center" }} >
+            <Layout className="layout">
+        <Sider trigger={null} collapsible collapsed={collapsed}>
+          <div className="logo" />
+          <Menu
+              theme="dark"
+              mode="inline"
+              defaultSelectedKeys={['1']}
+              items={[
+                {
+                  key: '1',
+                  icon: <UserOutlined />,
+                  label: 'Profile',
+                  Link :<Link to="/profile"/>
+                  
+                },
+                {
+                  key: '2',
+                  icon: <VideoCameraOutlined />,
+                  label: 'nav 2',
+                },
+                {
+                  key: '3',
+                  icon: <UploadOutlined />,
+                  label: 'nav 3',
+                },
+              ]}
+          />
+        </Sider>
+        <Layout className="site-layout">
+          <Header
+              className="site-layout-background"
+              style={{
+                padding: 0,
+              }}
+          >
+            {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+              className: 'trigger',
+              onClick: () => setCollapsed(!collapsed),
+            })}
+          </Header>
+          <Content className="site-layout-background"  style={{  margin: '24px 16px',  padding: 24, }}>
+            Content
+          </Content>
+        </Layout>
+      </Layout>
             <Head>
                 <title>My Profile</title>
             </Head>
